9318b6f4cc8351336a0103029f32324485f4c7e7
Kevin Chan <kefin@makedostudio.com>
2013-04-24 01:30:05 -0700

    Bumped version to 0.2.5

--- minipylib/__init__.py
+++ minipylib/__init__.py
@@ -20,7 +20,7 @@ __all__ = [
     "AUTHORS"
 ]
 
-VERSION = "0.2.4"
+VERSION = "0.2.5"
 DESCRIPTION = """Minipylib is a small library of utility functions
 for developing python web applications."""
 AUTHORS = (
