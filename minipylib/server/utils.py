ba2c04acc6c6059e9866bfabae62e03c27650e90
Kevin Chan <kefin@makedostudio.com>
2014-09-01 04:32:36 -0700

    Renamed parameters in get_uid_gid and change_uid_gid to be less confusing.

--- minipylib/server/utils.py
+++ minipylib/server/utils.py
@@ -18,34 +18,41 @@ from __future__ import (absolute_import, unicode_literals)
 # ** http://github.com/lincolnloop/django-cpserver
 #######################################################################
 
-def get_uid_gid(uid, gid=None):
+def get_uid_gid(user, group=None):
     """Try to change UID and GID to the provided values.
-    UID and GID are given as names like 'nobody' not integer.
+    user and group are given as names like 'nobody' not integer.
 
     :Source: `http://mail.mems-exchange.org/durusmail/quixote-users/4940/1/ <http://mail.mems-exchange.org/durusmail/quixote-users/4940/1/>`_
+
+    :param user: user name
+    :param group: group name
+    :returns: (uid, gid)
     """
     import pwd, grp
-    uid, default_grp = pwd.getpwnam(uid)[2:4]
-    if gid is None:
+    uid, default_grp = pwd.getpwnam(user)[2:4]
+    if group is None:
         gid = default_grp
     else:
         try:
-            gid = grp.getgrnam(gid)[2]
+            gid = grp.getgrnam(group)[2]
         except KeyError:
             gid = default_grp
     return (uid, gid)
 
 
-def change_uid_gid(uid, gid=None):
+def change_uid_gid(user, group=None):
     """Try to change UID and GID to the provided values.
-    UID and GID are given as names like 'nobody' not integer.
+    user and group are given as names like 'nobody' not integer.
 
     :Source: `http://mail.mems-exchange.org/durusmail/quixote-users/4940/1/ <http://mail.mems-exchange.org/durusmail/quixote-users/4940/1/>`_
+
+    :param user: user name
+    :param group: group name
     """
     import os
     if not os.geteuid() == 0:
         # Do not try to change the gid/uid if not root.
         return
-    (uid, gid) = get_uid_gid(uid, gid)
+    (uid, gid) = get_uid_gid(user, group)
     os.setgid(gid)
     os.setuid(uid)
